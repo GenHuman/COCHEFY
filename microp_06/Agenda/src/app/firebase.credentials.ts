6fb6e31ab4cbbf742b40a8b8791ed77ce544e345
genhuman <100329967@alumnos.uc3m.es>
2019-03-26 21:07:24 +0100

    commit

---
+++ microp_06/Agenda/src/app/firebase.credentials.ts
@@ -0,0 +1,7 @@
+export const fireBaseConfig={
+apiKey: "AIzaSyCxizq-9ZDsPufiRfv-AAbS1Py1yHzI3Xc",
+ authDomain: "cochefy-8a0bf.firebaseapp.com",
+ databaseURL: "https://cochefy-8a0bf.firebaseio.com",
+ projectId: "cochefy-8a0bf",
+ storageBucket: "cochefy-8a0bf.appspot.com",
+messagingSenderId: "848617746620"};
---
+++ microp_06/Agenda/src/providers/firebase-db/firebase-db.ts
@@ -0,0 +1,35 @@
+
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase} from 'angularfire2/database';
+import {Contact} from '../../models/contact.model';
+
+/*
+  Generated class for the FirebaseDbProvider provkeyer.
+
+  See https://angular.io/gukeye/dependency-injection for more info on provkeyers
+  and Angular DI.
+*/
+@Injectable()
+export class FirebaseDbProvider {
+
+private contactsRef=this.afDB.list<Contact>('contacts');
+
+  constructor(public afDB: AngularFireDatabase) {
+    console.log('Hello FirebaseDbProvider Provkeyer');
+  }
+  
+  guardaContact(contact:Contact){
+    if (contact.key=='') {contact.key=""+Date.now();}
+    return this.afDB.database.ref('contacts/'+contact.key).set(contact);
+  }
+
+  delContact(key){
+    this.afDB.database.ref('contacts/'+key).remove();
+  }
+  
+  
+	getContacts(){
+	return this.contactsRef.valueChanges();
+	} 
+
+}
