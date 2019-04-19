4838c977f6cdeeff6dd93df39dff3ebfed9630aa
pcanas <pablocanas97@gmail.com>
2019-04-19 18:57:39 +0200

    anhadido p2

---
+++ P2_Cochefy/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LibretaContactosPage } from
+  '../pages/libreta-contactos/libreta-contactos';
+import { AcercaDePage } from '../pages/acerca-de/acerca-de';
+import { NuevoContactoPage } from '../pages/nuevo-contacto/nuevo-contacto';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import {ContactService} from '../services/contact.service';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FirebaseDbProvider } from '../providers/firebase-db/firebase-db';
+import { fireBaseConfig } from '../app/firebase.credentials';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LibretaContactosPage,
+    AcercaDePage,
+    NuevoContactoPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+	AngularFireModule.initializeApp(fireBaseConfig),
+	AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LibretaContactosPage,
+    AcercaDePage,
+    NuevoContactoPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ContactService,
+    FirebaseDbProvider
+  ]
+})
+export class AppModule { }
+
