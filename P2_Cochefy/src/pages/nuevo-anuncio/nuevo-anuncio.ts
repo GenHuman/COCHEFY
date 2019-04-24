6ca0dd8e7a6f2dfc0200473c1fba68c3707e4ca5
genhuman <100329967@alumnos.uc3m.es>
2019-04-24 20:57:38 +0200

    distancia y validaciones

--- P2_Cochefy/src/pages/nuevo-anuncio/nuevo-anuncio.ts
+++ P2_Cochefy/src/pages/nuevo-anuncio/nuevo-anuncio.ts
@@ -39,18 +39,50 @@ export class NuevoAnuncioPage {
 	}
 
 	confirmarOferta () {
-		console.log(
-		"nPersonas="+this.nPersonas+
-		",fSalida="+this.fSalida+
-		",fRecogida="+this.fRecogida+
-		",distancia="+this.distancia+
-		",asegurado="+this.asegurado+
-		",cancelacion="+this.cancelacion);
-		var anuncio = {id:"", nombreUsuario: this.username,nPersonas:this.nPersonas, fSalida:this.fSalida, fRecogida:this.fRecogida, localizacion:"", distancia:this.distancia, asegurado:this.asegurado, cancelacion:this.cancelacion, alquilado:false}
-		//alert(anuncio);
-		this.anuncioService.addAnuncio(anuncio);
-        this.navCtrl.setRoot(AnunciosPage);
+		let date1 = new Date(this.fSalida+"T00:00:00");
+		let date2 = new Date(this.fRecogida+"T00:00:00");
+		if(date2 > date1){
+			if(this.nPersonas!=undefined){
+			if(this.fSalida!=undefined){
+			if(this.fRecogida!=undefined){
+			if(this.localizacion!=undefined){
+			if(this.distancia!=undefined){
+				console.log(
+				"nPersonas="+this.nPersonas+
+				",fSalida="+this.fSalida+
+				",fRecogida="+this.fRecogida+
+				",localizacion="+this.localizacion+
+				",distancia="+this.distancia+
+				",asegurado="+this.asegurado+
+				",cancelacion="+this.cancelacion);
+				var anuncio = {id:"", nombreUsuario: this.username,nPersonas:this.nPersonas, fSalida:this.fSalida, fRecogida:this.fRecogida, localizacion:this.localizacion, distancia:this.distancia, asegurado:this.asegurado, cancelacion:this.cancelacion, alquilado:false}
+				//alert(anuncio);
+				this.anuncioService.addAnuncio(anuncio);
+				this.navCtrl.setRoot(AnunciosPage);
+			}else{
+				alert("Tienes que rellenar la distancia a recorrer en el viaje.")
+				//document.getElementById("personasInput").focus();
+				//document.getElementById("personasInput").parentNode.classList.add = "show";
 
+				//document.getElementById("personasInput").classList.add = "step";
+			}
+			}else{
+				alert("Tienes que rellenar el lugar de recogida del vehículo.")
+			}
+			}else{
+				alert("Tienes que rellenar la fecha de llegada del viaje.")
+			}
+			}else{
+				alert("Tienes que rellenar la fecha de salida del viaje.")
+			}
+			}else{
+				alert("Tienes que rellenar el número de personas.")
+			}
+
+			
+		}else{
+			alert("La fecha de recogida tiene que ser posterior a la fecha de salida.")
+		}
 	}
 
 }
